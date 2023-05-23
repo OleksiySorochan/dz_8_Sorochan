29578b6855a4c5b4b60567e76bb50d080111edfe
OleksiySorochan <oleksiy.sorochan@gmail.com>
2023-05-23 15:38:14 +0300

    004_commit_master

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def maine_function():
-    a = 2
-    b = 10
+    a = int(input('введіть сторону a: '))
+    b = int(input('введіть сторону b: '))
     p = (a + b) * 2
     s = a * b
     print(f"периметр дорівнює: {p}")
